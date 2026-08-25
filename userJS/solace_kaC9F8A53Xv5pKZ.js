0cda7d5518c7daabac742de964d75e1c1e8cc152
jmo072011 <136132929+jmo072011@users.noreply.github.com>
2026-08-25 07:58:15 -0500

    Update solace_kaC9F8A53Xv5pKZ.js

--- userJS/solace_kaC9F8A53Xv5pKZ.js
+++ userJS/solace_kaC9F8A53Xv5pKZ.js
@@ -1,4 +1,4 @@
-console.log("Owner profile environment loaded. All system overrides granted >:3");
+console.log("Owner profile environment loaded. All system overrides granted ");
 
 const userSessionConfiguration = {
     identityKey: "solace_kaC9F8A53Xv5pKZ",
